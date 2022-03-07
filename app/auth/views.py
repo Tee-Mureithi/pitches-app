c99eedc88a296a90defac940fb4f9518f3c38b74
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-03-07 14:35:17 +0300

    adds the requirments

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for, flash,request
-from flasklogin import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=estarmureithi@gmail.com
+export MAIL_PASSWORD=o7oKanyi
+python3.6 manage.py server
